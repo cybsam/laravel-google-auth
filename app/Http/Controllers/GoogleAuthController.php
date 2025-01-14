a80a148594511df05a85faf04bd38df9d26f590e
SHUVO HALDER <samshuvo00@gmail.com>
2025-01-15 02:06:35 +0600

    create method for this GoogleAuthRoute

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class GoogleAuthController extends Controller
 {
-    //
+    public function GoogleAuthRoute(){
+        return Socialite::driver('google')->redirect();
+    }
 }
